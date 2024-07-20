bf52479b7d6b6066b5fa227628d3ff4a169e8da2
Niranjan MD <107424211+niranjan-md@users.noreply.github.com>
2024-07-20 19:00:30 +0530

    Added interests
    Added UI/UX into interests string

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["Networking", "Server Management", "web designing", "Linux Administration", "web development"],
+    strings: ["UI/UX", "Networking", "Server Management", "web designing", "Linux Administration", "web development"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
